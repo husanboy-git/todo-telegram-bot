826a6cafd19b0397a218d5e18f8a3e72dc6a6b93
husanboy <hus19950319@gmail.com>
2024-09-21 18:38:43 +0900

    gradel and yaml config

---
+++ src/main/java/com/test/todo_bot/TodoBotApplication.java
@@ -0,0 +1,13 @@
+package com.test.todo_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoBotApplication.class, args);
+    }
+
+}
